19f2f8aed085b66dd6d11c1db72938332729b1c1
Jan Wirth <jottenwe@gmail.com>
2015-12-06 14:41:05 +0100

    Add some stats

---
+++ assets/js/statistics/header.js
@@ -0,0 +1,14 @@
+window.data.header = [
+    '2014 Q1',
+    '2014 Q2',
+    '2014 Q3',
+    '2014 Q4',
+
+    '2015 Q1',
+    '2015 Q2',
+
+    '2015 Q2 change on previous Y',
+    '2015 Q2 change on previous Q',
+
+    '2015 Q2 percent of total',
+]
