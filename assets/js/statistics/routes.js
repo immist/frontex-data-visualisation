0313310a9a48d77b3b9958836f8bae6db9a805b9
Jan Wirth <jottenwe@gmail.com>
2016-01-06 12:39:32 +0100

    Add routes data

---
+++ assets/js/statistics/routes.js
@@ -0,0 +1,28 @@
+window.data.routes = {}
+
+window.data.routes.total = {
+    "Western African route":
+        data: [114, 107],
+        coordinates: [0,0]
+    "Western Mediterranean route":
+        data: [3890, 1496],
+        coordinates: [0,0]
+    "Central Mediterranean route":
+        data: [60179, 53054],
+        coordinates: [0,0]
+    "Eastern Mediterranean route":
+        data: [68178, 8767],
+        coordinates: [0,0]
+    "Circular route from Albania to Greece":
+        data: [2727, 2142],
+        coordinates: [0,0]
+    "Black Sea route":
+        data: [0, 65],
+        coordinates: [0,0]
+    "Eastern borders route":
+        data: [506, 222],
+        coordinates: [0,0]
+    "Western Balkan route":
+        data: [34559, 3011],
+        coordinates: [0,0]
+}
